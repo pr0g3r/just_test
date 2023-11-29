2fbefa0d14468f1ba8044a3f7519a240a0a2e7e0
vova <vova@elixirgardens.co.uk>
2023-11-29 12:47:19 +0000

    Add new file 2.php

---
+++ 2.php
@@ -0,0 +1,11 @@
+<?php
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+// set_time_limit(40);
+// ini_set("memory_limit", "-1");
+
+echo "testing";
+
+?>
